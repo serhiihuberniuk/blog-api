a10c022ee1caa3a5954e0e1b490e49d8ec86ce76
Serhii <gubserj@gmail.com>
2021-06-25 12:26:15 +0300

    fIX(command.sql):comments added

--- postgres/command.sql
+++ postgres/command.sql
@@ -4,23 +4,27 @@ INSERT INTO users (id, name, email, created_at) VALUES
 SELECT * FROM users WHERE name = 'vasia';
 SELECT name FROM users WHERE id =  'd59c98ba-84af-44ed-919d-cbe875b95af3';
 
+/*returns names of users with titles of their posts*/
 SELECT u.name,  p.title
 FROM  users u
     LEFT JOIN posts p
         ON u.id = p.created_by
 ORDER BY u.name;
 
+/*returns posts with its comments*/
 SELECT p.id, c.id
 FROM posts p
     LEFT JOIN comments c
         ON p.id = c.post_id
 ORDER BY p.id;
 
+/*returns 5 names of users ordered by email */
 SELECT name
 FROM  users
 ORDER BY email
 LIMIT 5;
 
+/*returns names of users which have at least 1 comment*/
 SELECT u.name
 FROM  users u
     LEFT JOIN posts p
