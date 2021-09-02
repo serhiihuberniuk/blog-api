c565960e6375884453253946ef6d7001d2cdba1a
Serhii <gubserj@gmail.com>
2021-09-02 17:33:44 +0300

    fix(repository_tests)

--- repository/postgresql/user_test.go
+++ repository/postgresql/user_test.go
@@ -15,7 +15,9 @@ import (
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func createPostgresTestContainer(ctx context.Context) (func(), *repository.Repository, error) {
+func createPostgresTestContainer(t *testing.T, ctx context.Context) (*repository.Repository, func()) {
+	t.Helper()
+
 	dbName := "test_postgres_db"
 	port := "5432/tcp"
 	env := map[string]string{
@@ -36,36 +38,46 @@ func createPostgresTestContainer(ctx context.Context) (func(), *repository.Repos
 
 	container, err := testcontainers.GenericContainer(ctx, req)
 	if err != nil {
-		return nil, nil, fmt.Errorf("error occurred while creating container: %w", err)
-	}
+		t.Log(fmt.Errorf("error occurred while creating container: %w", err))
+		t.Fail()
 
-	terminateContainer := func() {
-		err = container.Terminate(ctx)
-		if err != nil {
-			panic(err)
-		}
+		return nil, nil
 	}
 
 	mappedPort, err := container.MappedPort(ctx, nat.Port(port))
 	if err != nil {
-		return terminateContainer, nil, fmt.Errorf("error occurred while getting port: %w", err)
+		t.Log(fmt.Errorf("error occurred while getting port: %w", err))
+		t.Fail()
+
+		return nil, nil
 	}
 
 	dbUrl := fmt.Sprintf("postgres://postgres:password@localhost:%s/%s?sslmode=disable", mappedPort.Port(), dbName)
 
 	pool, err := repository.NewPostgresDb(ctx, dbUrl, "../../init.sql")
 	if err != nil {
-		return terminateContainer, nil, fmt.Errorf("cannot create conn pool: %w", err)
+		t.Log(fmt.Errorf("cannot create conn pool: %w", err))
+		t.Fail()
+
+		return nil, nil
+	}
+
+	closeFunc := func() {
+		pool.Close()
+		err = container.Terminate(ctx)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	repo := &repository.Repository{
 		Db: pool,
 	}
 
-	return terminateContainer, repo, nil
+	return repo, closeFunc
 }
 
-func cleanUserTable(t *testing.T, pool *pgxpool.Pool, ctx context.Context) {
+func cleanUserTable(t *testing.T, ctx context.Context, pool *pgxpool.Pool) {
 	t.Helper()
 
 	_, err := pool.Exec(ctx, "delete from users")
@@ -77,16 +89,8 @@ func cleanUserTable(t *testing.T, pool *pgxpool.Pool, ctx context.Context) {
 func TestRepository_User(t *testing.T) {
 	ctx := context.Background()
 
-	terminateContainer, repo, err := createPostgresTestContainer(ctx)
-	if err != nil {
-		t.Log(fmt.Errorf("error occurred while creating Postgres test container: %w", err))
-		t.Fail()
-
-		return
-	}
-
-	defer terminateContainer()
-	defer repo.Db.Close()
+	repo, closeFunc := createPostgresTestContainer(t, ctx)
+	defer closeFunc()
 
 	creationTime := time.Now()
 	updatingTime := time.Now().Add(time.Minute)
@@ -175,9 +179,9 @@ func TestRepository_User(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			defer cleanUserTable(t, repo.Db, ctx)
+			defer cleanUserTable(t, ctx, repo.Db)
 
-			err = repo.CreateUser(ctx, tc.userToCreate)
+			err := repo.CreateUser(ctx, tc.userToCreate)
 			if err != nil {
 				if tc.errMessage != "" {
 					assert.Contains(t, err.Error(), tc.errMessage)
